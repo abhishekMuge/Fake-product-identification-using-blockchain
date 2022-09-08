4c78a51f05d8438b7d08a90d3ada818ef96358e9
abhishekmuge <muge111hhh@gmail.com>
2022-09-08 14:51:06 +0530

    implement pages

--- pages/transfer_product/index.js
+++ pages/transfer_product/index.js
@@ -1,7 +1,63 @@
 export default function transferProduct() {
   return (
     <div>
-      <h1>Transfer Product</h1>
+      <h1 className="w-full text-3xl font-bold">Transfer Product</h1>
+      <form className="mt-10 w-full">
+        <div class="relative mb-4">
+          <label for="name" className="leading-7 text-md text-gray-600">
+            Product ID
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          />
+        </div>
+
+        <div className="flex mb-4">
+          <div class="relative mb-4 mr-5 w-1/2">
+            <label for="name" className="leading-7 text-md text-gray-600">
+              Owner Addrees
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+
+          <div class="relative mb-4 mr-5 w-2/5">
+            <label for="name" className="leading-7 text-md text-gray-600">
+              New Owner Addrees
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+        </div>
+        <button className="bg-black rounded-lg text-white hover:bg-gray-800 hover:text-grey-600 py-4 w-full flex justify-center items-center">
+          <span className="mr-3 font-semibold leading-7">Transfer</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 -rotate-45"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
+            />
+          </svg>
+        </button>
+      </form>
     </div>
   );
 }
--- utils/Navbar.js
+++ utils/Navbar.js
@@ -64,26 +64,6 @@ const Navbar = () => (
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            stroke-width="2"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
-            />
-          </svg>
-          <span className="mx-4 font-medium">Sell Product</span>
-        </a>
-        <a
-          className="flex items-center justify-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-200 transform dark:text-gray-400 hover:bg-gray-200 dark:hover:bg-gray-700 dark:hover:text-gray-200 hover:text-gray-700"
-          href="#"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
             class="h-5 w-5"
             viewBox="0 0 20 20"
             fill="currentColor"
