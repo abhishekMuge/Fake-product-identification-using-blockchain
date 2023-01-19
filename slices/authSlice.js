98e0289936d17181b022376e42a98cc5303a9465
unknown <kmohdmoazzam70@gmail.com>
2023-01-19 12:44:28 +0530

    Added notifications

--- slices/authSlice.js
+++ slices/authSlice.js
@@ -1,6 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { notiAction } from "./notificationSlice";
+import { createSlice } from "@reduxjs/toolkit"; 
 import Router from "next/router";
+import Swal from 'sweetalert2'
 
 const authSlice = createSlice({
   name: "auth",
@@ -35,10 +35,6 @@ export const authActions = authSlice.actions;
 export const signup = (data) => {
   return async (dispatch) => {
     console.log("Pending")
-    dispatch(notiAction.enableNotification({
-      message: "Registering User !",
-      heading: "Pending"
-    }))
     const response = await fetch("http://localhost:8000/api/user/register", {
       method: "POST",
       body: JSON.stringify(data),
@@ -49,25 +45,25 @@ export const signup = (data) => {
     let json = await response.json();
     console.log(json);
     if (json.status == 400) {
-      dispatch(notiAction.enableNotification({
-        message: "User notification registeration failed !",
-        heading: "Failed"
-      }))
-      setTimeout(() => {
-        dispatch(notiAction.disableNotification());
-      }, 2000);
+      Swal.fire({
+        position: 'bottom-end',
+        icon: 'failed',
+        title: 'User Registration Failed',
+        showConfirmButton: false,
+        timer: 2000
+      })
       console.log(json.message)
 
       Router.push("/auth/login")
     } 
     else {
-      dispatch(notiAction.enableNotification({
-        message: "User registered successfully !",
-        heading: "Success"
-      }));
-      setTimeout(() => {
-        dispatch(notiAction.disableNotification());
-      }, 2000);
+      Swal.fire({
+        position: 'bottom-end',
+        icon: 'success',
+        title: 'User Registration Successfully',
+        showConfirmButton: false,
+        timer: 2000
+      })
       console.log("Success")
       Router.push("/auth/login")
     }
@@ -77,10 +73,12 @@ export const signup = (data) => {
 export const signin = (data) => {
   return async (dispatch) => {
     console.log("Logging in")
-    dispatch(notiAction.enableNotification({
-      message: "Loggin In",
-      heading: "Success"
-    }))
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Logging In',
+      showConfirmButton: false,
+    })
     console.log(data);
     let response = await fetch("http://localhost:8000/api/user/login", {
       method: "POST",
@@ -94,24 +92,24 @@ export const signin = (data) => {
     console.log(response)
     if (!response.ok) {
       console.log("User Login failed")
-      dispatch(notiAction.enableNotification({
-        message: "User Login failed !",
-        heading: "Failed"
-      }))
-      setTimeout(() => {
-        dispatch(notiAction.disableNotification());
-      }, 2000);
+      Swal.fire({
+        position: 'bottom-end',
+        icon: 'failed',
+        title: 'User Login Failed',
+        showConfirmButton: false,
+        timer: 2000
+      })
     } 
     else {
+      Swal.fire({
+        position: 'bottom-end',
+        icon: 'success',
+        title: 'User Logged In',
+        showConfirmButton: false,
+        timer: 2000
+      })
       console.log("User Login Successfull")
-      dispatch(notiAction.enableNotification({
-        message: "User login Successful !",
-        heading: "Success"
-      }))
       dispatch(authActions.login({ userData: json.data.user }));
-      setTimeout(() => {
-        dispatch(notiAction.disableNotification());
-      }, 2000);
       Router.push("/products");
     }
   };
--- store.js
+++ store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import notiReducer from "./slices/notificationSlice";
 import contractReducer from "./slices/contractSlice";
 
 export const store = configureStore({
   reducer: {
     contract: contractReducer.reducer,
-    auth: authReducer.reducer,
-    notiSlice: notiReducer.reducer,
+    auth: authReducer.reducer,    
   },
 });
