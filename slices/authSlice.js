f7ab70467f1b61c3d64351bae73a5705e7fa3ed2
abhishekmuge <muge111hhh@gmail.com>
2023-01-20 15:14:58 +0530

    solved minor conflict

--- slices/authSlice.js
+++ slices/authSlice.js
@@ -1,6 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit"; 
+import { createSlice } from "@reduxjs/toolkit";
 import Router from "next/router";
-import Swal from 'sweetalert2'
+import Swal from "sweetalert2";
 
 const authSlice = createSlice({
   name: "auth",
@@ -34,17 +34,7 @@ export const authActions = authSlice.actions;
 
 export const signup = (data) => {
   return async (dispatch) => {
-<<<<<<< HEAD
     console.log("Pending");
-    dispatch(
-      notiAction.enableNotification({
-        message: "Registering User !",
-        heading: "Pending",
-      })
-    );
-=======
-    console.log("Pending")
->>>>>>> 98e0289936d17181b022376e42a98cc5303a9465
     const response = await fetch("http://localhost:8000/api/user/register", {
       method: "POST",
       body: JSON.stringify(data),
@@ -54,80 +44,40 @@ export const signup = (data) => {
     });
     let json = await response.json();
     console.log(json);
-<<<<<<< HEAD
-    if (json.status == 401) {
-      alert("Password not matched!");
-      dispatch(
-        notiAction.enableNotification({
-          message: "User notification registeration failed !",
-          heading: "Failed",
-        })
-      );
-      setTimeout(() => {
-        dispatch(notiAction.disableNotification());
-      }, 2000);
+    if (json.status == 400) {
+      Swal.fire({
+        position: "bottom-end",
+        icon: "failed",
+        title: "User Registration Failed",
+        showConfirmButton: false,
+        timer: 2000,
+      });
       console.log(json.message);
 
       Router.push("/auth/login");
     } else {
-      dispatch(
-        notiAction.enableNotification({
-          message: "User registered successfully !",
-          heading: "Success",
-        })
-      );
-      setTimeout(() => {
-        dispatch(notiAction.disableNotification());
-      }, 2000);
+      Swal.fire({
+        position: "bottom-end",
+        icon: "success",
+        title: "User Registration Successfully",
+        showConfirmButton: false,
+        timer: 2000,
+      });
       console.log("Success");
       Router.push("/auth/login");
-=======
-    if (json.status == 400) {
-      Swal.fire({
-        position: 'bottom-end',
-        icon: 'failed',
-        title: 'User Registration Failed',
-        showConfirmButton: false,
-        timer: 2000
-      })
-      console.log(json.message)
-
-      Router.push("/auth/login")
-    } 
-    else {
-      Swal.fire({
-        position: 'bottom-end',
-        icon: 'success',
-        title: 'User Registration Successfully',
-        showConfirmButton: false,
-        timer: 2000
-      })
-      console.log("Success")
-      Router.push("/auth/login")
->>>>>>> 98e0289936d17181b022376e42a98cc5303a9465
     }
   };
 };
 
 export const signin = (data) => {
   return async (dispatch) => {
-<<<<<<< HEAD
     console.log("Logging in");
-    dispatch(
-      notiAction.enableNotification({
-        message: "Loggin In",
-        heading: "Success",
-      })
-    );
-=======
-    console.log("Logging in")
     Swal.fire({
-      position: 'top-end',
-      icon: 'success',
-      title: 'Logging In',
+      position: "top-end",
+      icon: "success",
+      title: "Logging In",
       showConfirmButton: false,
-    })
->>>>>>> 98e0289936d17181b022376e42a98cc5303a9465
+    });
     console.log(data);
     let response = await fetch("http://localhost:8000/api/user/login", {
       method: "POST",
@@ -140,45 +90,23 @@ export const signin = (data) => {
     console.log(json);
     console.log(response);
     if (!response.ok) {
-<<<<<<< HEAD
       console.log("User Login failed");
-      dispatch(
-        notiAction.enableNotification({
-          message: "User Login failed !",
-          heading: "Failed",
-        })
-      );
-      setTimeout(() => {
-        dispatch(notiAction.disableNotification());
-      }, 2000);
+      Swal.fire({
+        position: "bottom-end",
+        icon: "failed",
+        title: "User Login Failed",
+        showConfirmButton: false,
+        timer: 2000,
+      });
     } else {
+      Swal.fire({
+        position: "bottom-end",
+        icon: "success",
+        title: "User Logged In",
+        showConfirmButton: false,
+        timer: 2000,
+      });
       console.log("User Login Successfull");
-      dispatch(
-        notiAction.enableNotification({
-          message: "User login Successful !",
-          heading: "Success",
-        })
-      );
-=======
-      console.log("User Login failed")
-      Swal.fire({
-        position: 'bottom-end',
-        icon: 'failed',
-        title: 'User Login Failed',
-        showConfirmButton: false,
-        timer: 2000
-      })
-    } 
-    else {
-      Swal.fire({
-        position: 'bottom-end',
-        icon: 'success',
-        title: 'User Logged In',
-        showConfirmButton: false,
-        timer: 2000
-      })
-      console.log("User Login Successfull")
->>>>>>> 98e0289936d17181b022376e42a98cc5303a9465
       dispatch(authActions.login({ userData: json.data.user }));
       Router.push("/products");
     }
